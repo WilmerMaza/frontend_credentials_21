d74ae556d98ff64171623f6c7c294a97fdadb305
ren-wmaza <wmaza@rengroup.co>
2026-05-20 16:01:37 -0500

    chore: update enap_api base URL to point to static IP address

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  enap_api: 'http://api:3000',
+  enap_api: 'http://172.24.31.60:3000',
   GetHexaKey:
     '>LTzgS(-zReKb?QD+mwdqiyDXo*/<>2;eyqR5|p}I.oTtdZb:jMv5wqiH:3&:K5{FH,2w0H4?{>(|@!I98$gac',
 };
